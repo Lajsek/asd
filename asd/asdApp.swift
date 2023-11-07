a8e5ac1756a6ba8eb2f62f9669503b55e46e07c4
lajsek <karol.lis@pollub.edu.pl>
2023-11-07 12:56:12 +0100

    Initial Commit

---
+++ asd/asdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdApp.swift
+//  asd
+//
+//  Created by student on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct asdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
